5d0f9d565cc8aae28d8f87fd1f9630c630449d6a
AbarnaR7 <159643766+AbarnaR7@users.noreply.github.com>
2024-02-17 08:54:38 -0800

    Create printspecialcharacter.java

---
+++ printspecialcharacter.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	
+	String s="Abar12na@R*a#gu";
+	String str="";
+	int count=0;
+	for(int i=0;i<s.length();i++)
+	{
+	    if(!Character.isDigit(s.charAt(i))&&!Character.isLetter(s.charAt(i))&&!Character.isWhitespace(s.charAt(i)))
+	    {   count++;
+	        str=str+s.charAt(i);
+	    }
+	}
+	    System.out.println(count);
+       System.out.println(str);
+}
+}
