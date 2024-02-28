3650f83c7d74a812bffb6b026cee0ea6a4a6bbb4
AbarnaR7 <159643766+AbarnaR7@users.noreply.github.com>
2024-02-28 10:38:12 -0800

    Create Removevovels.java

---
+++ Removevovels.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    String s="helloworldi";
+	  String str= s.replaceAll("[aeiou]","");
+
+	    System.out.println(str);
+}
+
+}
