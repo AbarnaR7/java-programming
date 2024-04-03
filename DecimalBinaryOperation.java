090713b1b35a1256bd0b77d7a69f0c5903f0f4a1
AbarnaR7 <159643766+AbarnaR7@users.noreply.github.com>
2024-04-03 11:01:30 -0700

    Update DecimalBinaryOperation.java

--- DecimalBinaryOperation.java
+++ DecimalBinaryOperation.java
@@ -1,37 +1,34 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-   int a=50,r;
+   int a=5,r;
    ArrayList<Integer> b=new ArrayList<>();
       while(a>0)
       {
           r=a%2;
           b.add(r);
           a=a/2;
+      } 
+      if(a<8)
+      b.add(0);
+
+      Collections.reverse(b);
+           System.out.println(b);
+
+      for(int i=0;i<b.size();i++)
+      {
+          if(b.get(i)==0)
+          b.set(i,1);
+          else
+          b.set(i,0);
       }
-     int ans[]=new int[b.size()];
-     int k=0;
+      
+     System.out.println(b);
+     int sum=0,count=0;
      for(int i=b.size()-1;i>=0;i--)
      {
-         if(b.get(i)==0)
-         ans[k]=1;
-         else
-         ans[k]=0;
-         k++;
-     }
-     System.out.println(Arrays.toString(ans));
-     int sum=0,count=0;
-     for(int i=ans.length-1;i>=0;i--)
-     {
-        sum=sum+ans[i]*(int)Math.pow(2,count);
+        sum=sum+b.get(i)*(int)Math.pow(2,count);
         count++;
      }
           System.out.println(sum);
